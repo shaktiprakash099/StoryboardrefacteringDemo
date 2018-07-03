71a57d1f17f0aaa540d6b6d806b77e7ec9da72cf
shaktiprakash <shaktiprakash@globussoft.in>
2018-07-03 15:32:29 +0530

    InitialCommit

---
+++ RefacteringOfStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RefacteringOfStoryboard
+//
+//  Created by GLB-312-PC on 03/07/18.
+//  Copyright © 2018 IOSISFUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
